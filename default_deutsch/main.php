b16db34245e1d4d07cf70f80d6954dd8f18cd022
abolabo <abolabo@gmail.com>
2025-07-09 17:52:13 +0300

    deutch

---
+++ default_deutsch/main.php
@@ -0,0 +1,28 @@
+<?php
+
+if (! defined ( 'DIR_CORE' )) {
+ header ( 'Location: static_pages/' );
+}
+
+$controllers = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$models = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$templates = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$languages = [
+    'storefront' => ['default_deutsch/default_deutsch'],
+    'admin' => [
+        'default_deutsch/default_deutsch'
+    ]
+];
+
