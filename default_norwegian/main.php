2a69e3f5b8420d36f05b785216ad75c5ba59189c
abolabo <abolabo@gmail.com>
2025-08-19 00:37:26 +0300

    norwegian

---
+++ default_norwegian/main.php
@@ -0,0 +1,27 @@
+<?php
+if (! defined ( 'DIR_CORE' )) {
+ header ( 'Location: static_pages/' );
+}
+
+$controllers = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$models = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$templates = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$languages = [
+    'storefront' => ['default_norwegian/default_norwegian'],
+    'admin' => [
+        'english/default_norwegian/default_norwegian'
+    ]
+];
+
