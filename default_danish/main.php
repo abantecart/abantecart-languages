0d6d444bb29883a5a81b2efb4817493342c2c1ca
abolabo <abolabo@gmail.com>
2025-08-19 17:02:40 +0300

    danish

---
+++ default_danish/main.php
@@ -0,0 +1,29 @@
+<?php
+if (! defined ( 'DIR_CORE' )) {
+ header ( 'Location: static_pages/' );
+}
+
+$controllers = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$models = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$templates = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$languages = [
+    'storefront' => [
+        'default_danish/default_danish'
+    ],
+    'admin' => [
+        'default_danish/default_danish'
+    ]
+];
+
