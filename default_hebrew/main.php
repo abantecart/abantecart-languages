2a5871d8bcb098239a48c5e90571785ceb228057
abolabo <abolabo@gmail.com>
2025-08-26 17:50:51 +0300

    hebrew

---
+++ default_hebrew/main.php
@@ -0,0 +1,26 @@
+<?php
+if (! defined ( 'DIR_CORE' )) {
+ header ( 'Location: static_pages/' );
+}
+
+$controllers = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$models = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$templates = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$languages = [
+    'storefront' => ['default_hebrew/default_hebrew'],
+    'admin' => [
+        'default_hebrew/default_hebrew'
+    ]
+];
---
+++ default_hebrew/non_translatable.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  0 => false,
+  1 => 'ZenCart 1.3.9h',
+  2 => 'H:i',
+  3 => '+',
+  4 => '-',
+  5 => 'MIME:',
+  6 => 'IM',
+  7 => 'SMTP',
+);
