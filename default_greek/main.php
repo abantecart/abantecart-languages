c17c7cf96dd97870be0cbde4e208ba9859dde6c4
abolabo <abolabo@gmail.com>
2025-08-27 20:27:42 +0300

    greek

---
+++ default_greek/main.php
@@ -0,0 +1,31 @@
+<?php
+
+
+if (! defined ( 'DIR_CORE' )) {
+ header ( 'Location: static_pages/' );
+}
+
+$controllers = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$models = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$templates = [
+    'storefront' => [],
+    'admin' => []
+];
+
+$languages = [
+    'storefront' => [
+        'default_croatian/default_croatian'
+    ],
+    'admin' => [
+        'default_croatian/default_croatian'
+    ]
+];
+
